13c7ae1be7c6daae88886a3b728c099fd86f69d3
Jade Hayes <Jade.e.Hayes@gmail.com>
2019-02-21 20:00:14 -0800

    * docker.for.mac.host.internal again

--- frontend/src/components/TechStack.js
+++ frontend/src/components/TechStack.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 // TODO: in production, try using docker.for.mac.host.internal
 
+
 class TechStack extends Component {
   render() {
     return (
       <div className='main bubbles'>
-        <iframe width="100%" height="100%" border="0" margin="0" src="http://localhost:8080/tech-stack/"/>
+        <iframe width="100%" height="100%" border="0" margin="0" src="docker.for.mac.host.internal:8080/tech-stack/"/>
      </div>
     )}
   }
