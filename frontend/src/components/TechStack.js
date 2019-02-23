466a5ca64bb5a5d35b371c602ae37510dc88ce39
Jade Hayes <Jade.e.Hayes@gmail.com>
2019-02-22 17:36:10 -0800

    * removed .docker-compose.yml.swp

--- frontend/src/components/TechStack.js
+++ frontend/src/components/TechStack.js
@@ -7,7 +7,7 @@ class TechStack extends Component {
   render() {
     return (
       <div id='tech-stack'>
-        <iframe width="100%" height="100%" border="0" margin="0" src="http://docker.for.mac.host.internal:8080/tech-stack/"/>
+        <iframe width="100%" height="100%" border="0" margin="0" src="docker.for.mac.host.internal:8080/tech-stack/"/>
      </div>
     )}
   }
