c679af04d5c0c645ad163ad80023b49f59f9bed7
Jade Hayes <Jade.e.Hayes@gmail.com>
2019-02-22 17:08:34 -0800

    docker.for.mac.host.internal

--- frontend/src/components/TechStack.js
+++ frontend/src/components/TechStack.js
@@ -6,8 +6,8 @@ import React, { Component } from 'react';
 class TechStack extends Component {
   render() {
     return (
-      <div className='main bubbles' id='tech-stack'>
-        <iframe width="100%" height="100%" border="0" margin="0" src="http://localhost:8080/tech-stack/"/>
+      <div id='tech-stack'>
+        <iframe width="100%" height="100%" border="0" margin="0" src="http://docker.for.mac.host.internal:8080/tech-stack/"/>
      </div>
     )}
   }
