989b531eaa1d06768a5f5f22c1400a8ac26d1c6d
Jade Hayes <Jade.e.Hayes@gmail.com>
2019-02-26 23:24:12 -0800

    * http://0.0.0.0:8080/ <- added to iframe.

--- frontend/src/components/TechStack.js
+++ frontend/src/components/TechStack.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import os
 
-// TODO: in production, try using docker.for.mac.host.internal
-// host.docker.internal
+// TODO: in production, try using host.docker.internal
+
 
 class TechStack extends Component {
   render() {
     return (
       <div className='main' id='tech-stack'>
-        <iframe width="100%" height="100%" src="https://jadehayes.herokuapp.com:8081/tech-stack/"/>
+        <iframe width="100%" height="100%" src="http://0.0.0.0:8080/tech-stack/"/>
      </div>
     )}
   }
