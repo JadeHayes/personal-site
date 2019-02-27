7aebb5824f3966890966665f41abba1688c858b8
Jade Hayes <Jade.e.Hayes@gmail.com>
2019-02-27 02:21:23 -0800

    added port

--- frontend/src/components/TechStack.js
+++ frontend/src/components/TechStack.js
@@ -7,7 +7,7 @@ class TechStack extends Component {
   render() {
     return (
       <div className='main' id='tech-stack'>
-        <iframe width="100%" height="100%" src="http:/jadehayes.herokuapp/tech-stack/"/>
+        <iframe width="100%" height="100%" src="http:/jadehayes.herokuapp:8080/tech-stack/"/>
         <script>document.write()</script>
      </div>
     )}
