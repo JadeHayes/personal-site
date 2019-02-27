ab4c0b10f356f68ccf6a47601ddcf8d1bef6b66d
Jade Hayes <Jade.e.Hayes@gmail.com>
2019-02-27 01:09:14 -0800

    http

--- frontend/src/components/TechStack.js
+++ frontend/src/components/TechStack.js
@@ -7,7 +7,7 @@ class TechStack extends Component {
   render() {
     return (
       <div className='main' id='tech-stack'>
-        <iframe width="100%" height="100%" src="https://0.0.0.0:8080/tech-stack/"/>
+        <iframe width="100%" height="100%" src="http://0.0.0.0:8080/tech-stack/"/>
         <script>document.write()</script>
      </div>
     )}
