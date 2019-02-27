a54ffa0f4ee9d9648ea8ed046fa7c9290a150655
Jade Hayes <Jade.e.Hayes@gmail.com>
2019-02-26 22:52:55 -0800

    * attempt 1 nginx

--- frontend/src/components/TechStack.js
+++ frontend/src/components/TechStack.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import os
 
 // TODO: in production, try using docker.for.mac.host.internal
-
+// host.docker.internal
 
 class TechStack extends Component {
   render() {
     return (
       <div className='main' id='tech-stack'>
-        <iframe width="100%" height="100%" src="http://localhost:8080/tech-stack/"/>
+        <iframe width="100%" height="100%" src="https://jadehayes.herokuapp.com:8081/tech-stack/"/>
      </div>
     )}
   }
